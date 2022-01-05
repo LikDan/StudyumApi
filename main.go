486361ef08c4bb2ce2b06f0958b17f8d15541c70
LikDan <likdan.company@gmail.com>
2022-01-05 12:26:47 +0300

    Remove server cron

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/robfig/cron"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -34,10 +33,6 @@ func main() {
 
 	log.Println(os.Hostname())
 
-	resp, err := http.Get("https://kbp.by")
-	checkError(err)
-	log.Println(resp.StatusCode)
-
 	client, err := mongo.NewClient(options.Client().ApplyURI(getDbUrl()))
 	if err != nil {
 		log.Fatal(err)
@@ -53,14 +48,6 @@ func main() {
 
 	initFirebaseApp()
 	Launch()
-
-	serverCrone := cron.New()
-	err = serverCrone.AddFunc("@every 10m", func() {
-		_, err := http.Get("https://college-helper.herokuapp.com/")
-		checkError(err)
-	})
-	checkError(err)
-	serverCrone.Start()
 
 	http.HandleFunc("/schedule", getSchedule)
 	http.HandleFunc("/schedule/types", getScheduleTypes)
