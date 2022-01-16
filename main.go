17bab25002d5734f416a93ad23fae26b3f700510
LikDan <likdan.company@gmail.com>
2022-01-16 13:54:22 +0300

    Create getLogin func

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 	userGroup.GET("/edit", editUser)
 	userGroup.GET("/create", createUser)
 	userGroup.GET("/delete", deleteUser)
+	userGroup.GET("/getLogin", getLogin)
 
 	userGroup.GET("/schedule", getUserSchedule)
 
