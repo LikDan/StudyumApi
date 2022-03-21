afd6afaa78e4944e7d18ed1cc5052654aa289a37
LikCoD <likdan.company@gmail.com>
2022-03-21 12:07:55 -0700

    Change server requests (GET POST PUT DELETE)

--- src/api/journal/journal.go
+++ src/api/journal/journal.go
@@ -89,36 +89,19 @@ func addMark(ctx *gin.Context) {
 		return
 	}
 
-	mark_ := ctx.Query("mark")
-	userId := ctx.Query("userId")
-	subjectId := ctx.Query("subjectId")
-
-	if mark_ == "" || userId == "" || subjectId == "" {
-		h.ErrorMessage(ctx, "provide valid params")
-		return
-	}
-
-	userObjectId, err := primitive.ObjectIDFromHex(userId)
-
+	var mark Mark
+	err = ctx.BindJSON(&mark)
 	if h.CheckError(err) {
+		h.ErrorMessage(ctx, err.Error())
+		return
+	}
+
+	if mark.Mark == "" || mark.UserId.IsZero() || mark.SubjectId.IsZero() {
 		h.ErrorMessage(ctx, "provide valid params")
 		return
 	}
 
-	subjectObjectId, err := primitive.ObjectIDFromHex(subjectId)
-
-	if h.CheckError(err) {
-		h.ErrorMessage(ctx, "provide valid params")
-		return
-	}
-
-	mark := Mark{
-		Id:           primitive.NewObjectID(),
-		Mark:         mark_,
-		SubjectId:    subjectObjectId,
-		UserId:       userObjectId,
-		StudyPlaceId: user.StudyPlaceId,
-	}
+	mark.Id = primitive.NewObjectID()
 
 	_, err = db.MarksCollection.InsertOne(nil, mark)
 	if h.CheckError(err) {
@@ -126,7 +109,7 @@ func addMark(ctx *gin.Context) {
 		return
 	}
 
-	marks := getMarksViaId(userObjectId, subjectObjectId)
+	marks := getMarksViaId(mark.UserId, mark.SubjectId)
 
 	if len(marks) != 1 {
 		h.ErrorMessage(ctx, "wrong response")
@@ -174,43 +157,25 @@ func editMark(ctx *gin.Context) {
 		return
 	}
 
-	mark_ := ctx.Query("mark")
-	markId := ctx.Query("markId")
-	userIdHex := ctx.Query("userId")
-	subjectId := ctx.Query("subjectId")
-
-	if mark_ == "" || markId == "" || userIdHex == "" || subjectId == "" {
-		h.ErrorMessage(ctx, "provide valid params")
-		return
-	}
-
-	userId, err := primitive.ObjectIDFromHex(userIdHex)
+	var mark Mark
+	err = ctx.BindJSON(&mark)
 	if h.CheckError(err) {
 		h.ErrorMessage(ctx, err.Error())
 		return
 	}
 
-	markObjectId, err := primitive.ObjectIDFromHex(markId)
-
-	if h.CheckError(err) {
+	if mark.Mark == "" || mark.Id.IsZero() || mark.UserId.IsZero() || mark.SubjectId.IsZero() {
 		h.ErrorMessage(ctx, "provide valid params")
 		return
 	}
 
-	subjectObjectId, err := primitive.ObjectIDFromHex(subjectId)
-
-	if h.CheckError(err) {
-		h.ErrorMessage(ctx, "provide valid params")
-		return
-	}
-
-	_, err = db.MarksCollection.UpdateOne(nil, bson.M{"_id": markObjectId, "subjectId": subjectObjectId}, bson.M{"$set": bson.M{"mark": mark_}})
+	_, err = db.MarksCollection.UpdateOne(nil, bson.M{"_id": mark.Id, "subjectId": mark.SubjectId}, bson.M{"$set": bson.M{"mark": mark.Mark}})
 	if h.CheckError(err) {
 		h.ErrorMessage(ctx, err.Error())
 		return
 	}
 
-	marks := getMarksViaId(userId, subjectObjectId)
+	marks := getMarksViaId(mark.UserId, mark.SubjectId)
 
 	if len(marks) != 1 {
 		h.ErrorMessage(ctx, "wrong response")
