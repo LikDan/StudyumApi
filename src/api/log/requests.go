c83c04502df8f6676bc3378d5d6d2761b99927f0
LikCoD <likdan.company@gmail.com>
2022-03-23 04:44:31 -0700

    Create request api

---
+++ src/api/log/requests.go
@@ -0,0 +1,19 @@
+package log
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+	h "studyium/api"
+)
+
+func getRequests(ctx *gin.Context) {
+	r, err := os.Open(ginWriter.File.Name())
+	if err != nil {
+		print(err.Error())
+	}
+
+	var logs []RequestInfo
+	h.DecodeJsonLines(r, &logs)
+
+	ctx.JSON(200, logs)
+}
