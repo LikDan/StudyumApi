8e33c1c6492d9dd8540246418de22e5ccd3722d7
LikCoD <likdan.company@gmail.com>
2022-04-08 02:03:27 -0700

    Improve user api

--- src/api/user/user.go
+++ src/api/user/user.go
@@ -1,52 +1,26 @@
 package user
 
 import (
-	"encoding/json"
 	"errors"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
-	"io/ioutil"
-	"net/http"
 	h "studyium/src/api"
 	"studyium/src/db"
 )
 
 func GetUserViaGoogle(ctx *gin.Context, user *User) error {
 	token, err := ctx.Cookie("authToken")
-
 	if h.CheckError(err, h.UNDEFINED) {
 		return errors.New("not authorized")
 	}
 
-	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token)
-	if h.CheckError(err, h.UNDEFINED) || response.StatusCode != 200 {
-		return errors.New("not authorized")
-	}
-
-	defer response.Body.Close()
-
-	content, err := ioutil.ReadAll(response.Body)
+	err = db.UsersCollection.FindOne(nil, bson.M{"token": token}).Decode(&user)
 	if h.CheckError(err, h.WARNING) {
-		return errors.New("bad callback")
-	}
-
-	var googleUser Google
-	err = json.Unmarshal(content, &googleUser)
-	if h.CheckError(err, h.WARNING) {
-		return errors.New("bad callback")
-	}
-
-	_, err = db.UsersCollection.InsertOne(nil, googleUser)
-	if err != nil {
-		_, err = db.UsersCollection.UpdateOne(nil, bson.M{"_id": googleUser.Id}, bson.M{"$set": googleUser})
-		if h.CheckError(err, h.WARNING) {
+		if err.Error() == "mongo: no documents in result" {
+			return errors.New("not authorized")
+		} else {
 			return err
 		}
-	}
-
-	err = db.UsersCollection.FindOne(nil, bson.M{"_id": googleUser.Id}).Decode(&user)
-	if h.CheckError(err, h.WARNING) {
-		return err
 	}
 
 	return nil
@@ -73,7 +47,7 @@ func updateUser(ctx *gin.Context) {
 		return
 	}
 
-	if user.Applied && !h.SliceContains(user.Permissions, "editInfo") {
+	if user.Accepted && !h.SliceContains(user.Permissions, "editInfo") {
 		h.ErrorMessage(ctx, "You don't have permission to edit information")
 		return
 	}
@@ -88,7 +62,8 @@ func updateUser(ctx *gin.Context) {
 	user.TypeName = userUpdate.TypeName
 	user.Name = userUpdate.Name
 
-	user.Applied = false
+	user.Accepted = false
+	user.Blocked = false
 
 	_, err := db.UsersCollection.UpdateOne(nil, bson.M{"_id": user.Id}, bson.M{"$set": user})
 	if h.CheckAndMessage(ctx, 500, err, h.WARNING) {
@@ -103,8 +78,93 @@ func updateUser(ctx *gin.Context) {
 	ctx.JSON(200, user)
 }
 
+func toAccept(ctx *gin.Context) {
+	var user User
+	if err := GetUserViaGoogle(ctx, &user); h.CheckAndMessage(ctx, 418, err, h.UNDEFINED) {
+		return
+	}
+
+	if !h.SliceContains(user.Permissions, "acceptUsers") {
+		h.Message(ctx, 403, "You don't have permission to accept users")
+		return
+	}
+
+	find, err := db.UsersCollection.Find(nil, bson.M{"studyPlaceId": user.StudyPlaceId, "accepted": false, "blocked": false})
+	if h.CheckAndMessage(ctx, 500, err, h.WARNING) {
+		return
+	}
+
+	var users []User
+	err = find.All(nil, &users)
+
+	ctx.JSON(200, users)
+}
+
+func accept(ctx *gin.Context) {
+	var user User
+	if err := GetUserViaGoogle(ctx, &user); h.CheckAndMessage(ctx, 418, err, h.UNDEFINED) {
+		return
+	}
+
+	if !h.SliceContains(user.Permissions, "acceptUsers") {
+		h.Message(ctx, 403, "You don't have permission to accept users")
+		return
+	}
+
+	var userId string
+	if err := ctx.Bind(&userId); h.CheckAndMessage(ctx, 500, err, h.WARNING) {
+		return
+	}
+
+	_, err := db.UsersCollection.UpdateOne(nil, bson.M{"_id": userId}, bson.M{"$set": bson.M{"accepted": true}})
+	if h.CheckAndMessage(ctx, 500, err, h.WARNING) {
+		return
+	}
+
+	h.Message(ctx, 200, "successful")
+}
+
+func decline(ctx *gin.Context) {
+	var user User
+	if err := GetUserViaGoogle(ctx, &user); h.CheckAndMessage(ctx, 418, err, h.UNDEFINED) {
+		return
+	}
+
+	if !h.SliceContains(user.Permissions, "acceptUsers") {
+		h.Message(ctx, 403, "You don't have permission to accept users")
+		return
+	}
+
+	var userId string
+	if err := ctx.Bind(&userId); h.CheckAndMessage(ctx, 500, err, h.WARNING) {
+		return
+	}
+
+	_, err := db.UsersCollection.UpdateOne(nil, bson.M{"_id": userId}, bson.M{"$set": bson.M{"blocked": true}})
+	if h.CheckAndMessage(ctx, 500, err, h.WARNING) {
+		return
+	}
+
+	h.Message(ctx, 200, "successful")
+}
+
+func revokeToken(ctx *gin.Context) {
+	token, err := ctx.Cookie("authToken")
+	if h.CheckAndMessage(ctx, 418, err, h.UNDEFINED) {
+		return
+	}
+
+	_, err = db.UsersCollection.UpdateOne(nil, bson.M{"token": token}, bson.M{"$set": bson.M{"token": ""}})
+	if h.CheckAndMessage(ctx, 500, err, h.WARNING) {
+		return
+	}
+
+	h.Message(ctx, 200, "successful")
+}
+
 type User struct {
 	Id            string   `json:"id" bson:"_id"`
+	Token         string   `json:"-" bson:"token"`
 	Email         string   `json:"email" bson:"email"`
 	VerifiedEmail bool     `json:"verifiedEmail" bson:"verifiedEmail"`
 	Login         string   `json:"login" bson:"login"`
@@ -114,7 +174,8 @@ type User struct {
 	TypeName      string   `json:"typeName" bson:"typeName"`
 	StudyPlaceId  int      `json:"studyPlaceId" bson:"studyPlaceId"`
 	Permissions   []string `json:"permissions" bson:"permissions"`
-	Applied       bool     `json:"applied" bson:"applied"`
+	Accepted      bool     `json:"accepted" bson:"accepted"`
+	Blocked       bool     `json:"blocked" bson:"blocked"`
 }
 
 func BuildRequests(api *gin.RouterGroup) {
@@ -122,8 +183,11 @@ func BuildRequests(api *gin.RouterGroup) {
 	api.GET("/logout", logout)
 	api.GET("/callback", callbackHandler)
 
-	api.PUT("", updateUser)
-
 	api.GET("", getUser)
+	api.PUT("", updateUser)
+	api.PUT("revoke", revokeToken)
 
+	api.GET("/toAccept", toAccept)
+	api.PUT("/accept", accept)
+	api.PUT("/decline", decline)
 }
