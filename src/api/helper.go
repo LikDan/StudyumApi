f55776bb53fc69f1aa17e079ba54edc85af420e1
LikCoD <likdan.company@gmail.com>
2022-04-22 18:16:45 +0300

    Create on site registration

--- src/api/helper.go
+++ src/api/helper.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/robfig/cron"
@@ -170,4 +173,17 @@ func BindShift(sHour, sMinute, eHour, eMinute int) Shift {
 		Start: GetTimeDuration(sHour, sMinute),
 		End:   GetTimeDuration(eHour, eMinute),
 	}
+}
+
+func GenerateSecureToken() string {
+	b := make([]byte, 128)
+	if _, err := rand.Read(b); err != nil {
+		return ""
+	}
+	return hex.EncodeToString(b)
+}
+
+func Hash(s string) string {
+	hash := md5.Sum([]byte(s))
+	return hex.EncodeToString(hash[:])
 }
--- src/api/user/user.go
+++ src/api/user/user.go
@@ -4,8 +4,11 @@ import (
 	"errors"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"net/http"
 	h "studyium/src/api"
 	"studyium/src/db"
+	"time"
 )
 
 func GetUserViaGoogle(ctx *gin.Context, user *User) error {
@@ -35,10 +38,79 @@ func getUser(ctx *gin.Context) {
 	ctx.JSON(200, user)
 }
 
+func createUser(ctx *gin.Context) {
+	var userData CreateUserData
+	if err := ctx.BindJSON(&userData); h.CheckAndMessage(ctx, 418, err, h.UNDEFINED) {
+		return
+	}
+
+	if !h.CheckNotEmpty(userData.Email, userData.Login, userData.Name, userData.TypeName, userData.Type, userData.Password) || userData.PasswordRepeat != userData.Password || len(userData.Password) < 8 {
+		h.ErrorMessage(ctx, "Provide valid params")
+		return
+	}
+
+	user := User{
+		Id:            primitive.NewObjectID(),
+		Token:         h.GenerateSecureToken(),
+		Password:      h.Hash(userData.Password),
+		Email:         userData.Email,
+		VerifiedEmail: false,
+		Login:         userData.Login,
+		Name:          userData.Name,
+		PictureUrl:    "https://i.stack.imgur.com/l60Hf.png",
+		Type:          userData.Type,
+		TypeName:      userData.TypeName,
+		StudyPlaceId:  userData.StudyPlaceId,
+		Permissions:   nil,
+		Accepted:      false,
+		Blocked:       false,
+	}
+
+	if _, err := db.UsersCollection.InsertOne(nil, user); h.CheckAndMessage(ctx, 418, err, h.WARNING) {
+		return
+	}
+
+	ctx.JSON(200, user)
+}
+
 func logout(ctx *gin.Context) {
 	ctx.SetCookie("authToken", "", -1, "", "", false, false)
 
 	h.Message(ctx, 200, "successful")
+}
+
+func login(ctx *gin.Context) {
+	var data = struct {
+		Email    string `json:"email" bson:"email"`
+		Password string `json:"password" bson:"password"`
+	}{}
+
+	if err := ctx.BindJSON(&data); h.CheckAndMessage(ctx, 418, err, h.WARNING) {
+		return
+	}
+
+	data.Password = h.Hash(data.Password)
+	var user User
+	if err := db.UsersCollection.FindOne(nil, data).Decode(&user); h.CheckAndMessage(ctx, 418, err, h.WARNING) {
+		return
+	}
+
+	if user.Token == "" {
+		user.Token = h.GenerateSecureToken()
+		if _, err := db.UsersCollection.UpdateOne(ctx, bson.M{"email": user.Email}, bson.M{"$set": bson.M{"token": user.Token}}); err != nil {
+			h.ErrorMessage(ctx, "cannot update user")
+			return
+		}
+	}
+
+	http.SetCookie(ctx.Writer, &http.Cookie{
+		Name:    "authToken",
+		Value:   user.Token,
+		Path:    "/",
+		Expires: time.Now().AddDate(1, 0, 0),
+	})
+
+	ctx.JSON(200, user)
 }
 
 func updateUser(ctx *gin.Context) {
@@ -163,19 +235,32 @@ func revokeToken(ctx *gin.Context) {
 }
 
 type User struct {
-	Id            string   `json:"id" bson:"_id"`
-	Token         string   `json:"-" bson:"token"`
-	Email         string   `json:"email" bson:"email"`
-	VerifiedEmail bool     `json:"verifiedEmail" bson:"verifiedEmail"`
-	Login         string   `json:"login" bson:"login"`
-	Name          string   `json:"name" bson:"name"`
-	PictureUrl    string   `json:"picture" bson:"picture"`
-	Type          string   `json:"type" bson:"type"`
-	TypeName      string   `json:"typeName" bson:"typeName"`
-	StudyPlaceId  int      `json:"studyPlaceId" bson:"studyPlaceId"`
-	Permissions   []string `json:"permissions" bson:"permissions"`
-	Accepted      bool     `json:"accepted" bson:"accepted"`
-	Blocked       bool     `json:"blocked" bson:"blocked"`
+	Id            primitive.ObjectID `json:"id" bson:"_id"`
+	Token         string             `json:"-" bson:"token"`
+	Password      string             `json:"password" bson:"password"`
+	Email         string             `json:"email" bson:"email"`
+	VerifiedEmail bool               `json:"verifiedEmail" bson:"verifiedEmail"`
+	Login         string             `json:"login" bson:"login"`
+	Name          string             `json:"name" bson:"name"`
+	PictureUrl    string             `json:"picture" bson:"picture"`
+	Type          string             `json:"type" bson:"type"`
+	TypeName      string             `json:"typeName" bson:"typeName"`
+	StudyPlaceId  int                `json:"studyPlaceId" bson:"studyPlaceId"`
+	Permissions   []string           `json:"permissions" bson:"permissions"`
+	Accepted      bool               `json:"accepted" bson:"accepted"`
+	Blocked       bool               `json:"blocked" bson:"blocked"`
+}
+
+type CreateUserData struct {
+	Id             string `json:"id"`
+	Email          string `json:"email"`
+	Login          string `json:"login"`
+	Name           string `json:"name"`
+	Type           string `json:"type"`
+	TypeName       string `json:"typeName"`
+	StudyPlaceId   int    `json:"studyPlaceId"`
+	Password       string `json:"password"`
+	PasswordRepeat string `json:"passwordRepeat"`
 }
 
 func BuildRequests(api *gin.RouterGroup) {
@@ -185,8 +270,12 @@ func BuildRequests(api *gin.RouterGroup) {
 
 	api.GET("", getUser)
 	api.PUT("", updateUser)
-	api.PUT("revoke", revokeToken)
-	api.PUT("token", putToken)
+	api.POST("", createUser)
+
+	api.PUT("/login", login)
+
+	api.PUT("/revoke", revokeToken)
+	api.PUT("/token", putToken)
 
 	api.GET("/toAccept", toAccept)
 	api.PUT("/accept", accept)
