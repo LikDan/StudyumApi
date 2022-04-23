a93ec65a2efef197f535ac0fa84bc16e9ec4762e
LikCoD <likdan.company@gmail.com>
2022-04-23 12:07:53 +0300

    Fix old schedule

--- src/api/schedule/lesson.go
+++ src/api/schedule/lesson.go
@@ -5,13 +5,21 @@ import (
 )
 
 type LessonOld struct {
-	Id          int        `bson:"_id" json:"-"`
-	Subjects    []*Subject `bson:"subjects" json:"subjects"`
-	ColumnIndex int32      `bson:"columnIndex" json:"columnIndex"`
-	RowIndex    int32      `bson:"rowIndex" json:"rowIndex"`
-	WeekIndex   int32      `bson:"weekIndex" json:"weekIndex"`
-	Date        time.Time  `bson:"date" json:"-"`
-	IsStay      bool       `bson:"isStay" json:"isStay"`
+	Id          int           `bson:"_id" json:"-"`
+	Subjects    []*SubjectOld `bson:"subjects" json:"subjects"`
+	ColumnIndex int32         `bson:"columnIndex" json:"columnIndex"`
+	RowIndex    int32         `bson:"rowIndex" json:"rowIndex"`
+	WeekIndex   int32         `bson:"weekIndex" json:"weekIndex"`
+	Date        time.Time     `bson:"date" json:"-"`
+	IsStay      bool          `bson:"isStay" json:"isStay"`
+}
+
+type SubjectOld struct {
+	Subject string `bson:"subject" json:"subject"`
+	Teacher string `bson:"teacher" json:"teacher"`
+	Group   string `bson:"group" json:"group"`
+	Room    string `bson:"room" json:"room"`
+	Type_   string `bson:"type" json:"type"`
 }
 
 type Lesson struct {
