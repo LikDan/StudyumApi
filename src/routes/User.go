650093f6d8fc105709c7c2c5aaa412bf97f73300
LikDan <likdan.company@gmail.com>
2022-06-05 02:31:19 +0300

    Improve user sign-in

---
+++ src/routes/User.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"studyum/src/api/user"
+	"studyum/src/controllers"
+)
+
+func User(root *gin.RouterGroup) {
+	root.PUT("login", controllers.LoginUser)
+	root.POST("signup", controllers.SignUpUser)
+
+	root.PUT("signup/stage1", controllers.SignUpUserStage1)
+
+	user.BuildRequests(root)
+}
