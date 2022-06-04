0efae63527ee7126bef2e5f16ae27d1f07b9d170
LikDan <likdan.company@gmail.com>
2022-06-05 00:36:43 +0300

    Fix

--- src/db/General.go
+++ src/db/General.go
@@ -1,19 +1,21 @@
 package db
 
 import (
+	"go.mongodb.org/mongo-driver/bson"
 	h "studyum/src/api"
 	"studyum/src/models"
 )
 
-func GetStudyPlaces(studyPlaces *[]models.StudyPlace) *models.Error {
-	studyPlacesCursor, err := StudyPlacesCollection.Find(nil, nil)
+func GetStudyPlaces() (*models.Error, []*models.StudyPlace) {
+	var studyPlaces []*models.StudyPlace
+	studyPlacesCursor, err := StudyPlacesCollection.Find(nil, bson.M{})
 	if err != nil {
-		return models.BindError(err, 418, h.WARNING)
+		return models.BindError(err, 418, h.WARNING), nil
 	}
 
 	if err := studyPlacesCursor.All(nil, &studyPlaces); err != nil {
-		return models.BindError(err, 418, h.WARNING)
+		return models.BindError(err, 418, h.WARNING), nil
 	}
 
-	return models.EmptyError()
+	return models.EmptyError(), studyPlaces
 }
