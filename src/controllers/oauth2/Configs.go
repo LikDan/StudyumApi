90c6f0481a6bd5d96eff16941c8f3102778af412
LikDan <likdan.company@gmail.com>
2022-06-07 16:10:35 +0300

    Improve oauth2 config

--- src/controllers/oauth2/Configs.go
+++ src/controllers/oauth2/Configs.go
@@ -3,13 +3,14 @@ package oauth2
 import (
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"os"
 )
 
 var googleOAuthConfig = &oauth2.Config{
-	ClientID:     "314976404425-5774o9r2j56p724ohicfegm6g4b2ch1t.apps.googleusercontent.com", //https://console.cloud.google.com/apis/dashboard
-	ClientSecret: "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",
+	ClientID:     "314976404425-5774o9r2j56p724ohicfegm6g4b2ch1t.apps.googleusercontent.com", //https	://console.cloud.google.com/apis/dashboard
+	ClientSecret: os.Getenv("GOOGLE_SECRET"),
 	Endpoint:     google.Endpoint,
-	RedirectURL:  "http://localhost:8080/api/user/callback",
+	RedirectURL:  os.Getenv("OAUTH2_CALLBACK") + "/api/user/callback",
 	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
 }
 
