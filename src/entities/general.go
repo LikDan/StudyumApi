2718e9204b72354d5647efa618382a2a19001102
LikDan <likdan.company@gmail.com>
2022-08-17 12:47:49 -0700

    Move entities to clean architecture

---
+++ src/entities/general.go
@@ -0,0 +1,8 @@
+package entities
+
+type StudyPlace struct {
+	Id         int    `json:"id" bson:"_id"`
+	WeeksCount int    `json:"weeksCount" bson:"weeksCount"`
+	DaysCount  int    `json:"daysCount" bson:"daysCount"`
+	Name       string `json:"name" bson:"name"`
+}
