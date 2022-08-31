78f722215e0523f53b21325e439a3b68f32a86b6
LikDan <likdan.company@gmail.com>
2022-08-31 05:33:14 -0700

    Add color sets

--- internal/entities/general.go
+++ internal/entities/general.go
@@ -1,8 +1,10 @@
 package entities
 
 type StudyPlace struct {
-	Id         int    `json:"id" bson:"_id"`
-	WeeksCount int    `json:"weeksCount" bson:"weeksCount"`
-	DaysCount  int    `json:"daysCount" bson:"daysCount"`
-	Name       string `json:"name" bson:"name"`
+	Id                int      `json:"id" bson:"_id"`
+	WeeksCount        int      `json:"weeksCount" bson:"weeksCount"`
+	DaysCount         int      `json:"daysCount" bson:"daysCount"`
+	Name              string   `json:"name" bson:"name"`
+	PrimaryColorSet   []string `json:"primaryColorSet" bson:"primaryColorSet"`
+	SecondaryColorSet []string `json:"secondaryColorSet" bson:"secondaryColorSet"`
 }
