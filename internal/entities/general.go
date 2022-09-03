bb026e36ccb171a632579083d9f407bd54ad2229
LikDan <likdan.company@gmail.com>
2022-09-02 21:36:28 -0700

    Change study place id from id to objectID

--- internal/entities/general.go
+++ internal/entities/general.go
@@ -1,11 +1,13 @@
 package entities
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type StudyPlace struct {
-	Id                int      `json:"id" bson:"_id"`
-	WeeksCount        int      `json:"weeksCount" bson:"weeksCount"`
-	DaysCount         int      `json:"daysCount" bson:"daysCount"`
-	Name              string   `json:"name" bson:"name"`
-	PrimaryColorSet   []string `json:"primaryColorSet" bson:"primaryColorSet"`
-	SecondaryColorSet []string `json:"secondaryColorSet" bson:"secondaryColorSet"`
-	Restricted        bool     `json:"restricted" bson:"restricted"`
+	Id                primitive.ObjectID `json:"id" bson:"_id"`
+	WeeksCount        int                `json:"weeksCount" bson:"weeksCount"`
+	DaysCount         int                `json:"daysCount" bson:"daysCount"`
+	Name              string             `json:"name" bson:"name"`
+	PrimaryColorSet   []string           `json:"primaryColorSet" bson:"primaryColorSet"`
+	SecondaryColorSet []string           `json:"secondaryColorSet" bson:"secondaryColorSet"`
+	Restricted        bool               `json:"restricted" bson:"restricted"`
 }
