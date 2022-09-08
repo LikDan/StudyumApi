fe4bbecbeb655d6b1c344503132a39b5e9c63b87
LikDan <likdan.company@gmail.com>
2022-09-08 17:01:14 +0300

    Edit schedule query

--- internal/repositories/scheduleRepository.go
+++ internal/repositories/scheduleRepository.go
@@ -35,8 +35,8 @@ func (s *scheduleRepository) GetStudyPlaceByID(ctx context.Context, id primitive
 	return
 }
 
-func (s *scheduleRepository) GetSchedule(ctx context.Context, studyPlaceId primitive.ObjectID, type_ string, typeName string, asPreview bool) (entities.Schedule, error) {
-	filter := bson.M{"_id": studyPlaceId}
+func (s *scheduleRepository) GetSchedule(ctx context.Context, studyPlaceID primitive.ObjectID, type_ string, typeName string, asPreview bool) (entities.Schedule, error) {
+	filter := bson.M{"_id": studyPlaceID}
 	if asPreview {
 		filter["restricted"] = false
 	}
@@ -44,46 +44,146 @@ func (s *scheduleRepository) GetSchedule(ctx context.Context, studyPlaceId primi
 	startWeekDate := datetime.Date().AddDate(0, 0, 1-int(time.Now().Weekday()))
 	cursor, err := s.studyPlacesCollection.Aggregate(ctx, bson.A{
 		bson.M{"$match": filter},
-		bson.M{"$addFields": bson.M{"date": bson.M{"$range": bson.A{0, bson.M{"$multiply": bson.A{7, "$weeksCount"}}}}}},
-		bson.M{"$unwind": "$date"},
 		bson.M{
 			"$addFields": bson.M{
-				"date": bson.M{"$dateAdd": bson.M{
-					"startDate": startWeekDate,
-					"unit":      "day",
-					"amount":    "$date",
-				}},
-			},
-		},
-		bson.M{
-			"$addFields": bson.M{
-				"indexes": bson.M{
-					"weekIndex": bson.M{"$mod": bson.A{bson.M{"$isoWeek": "$date"}, "$weeksCount"}},
-					"dayIndex":  bson.M{"$subtract": bson.A{bson.M{"$isoDayOfWeek": "$date"}, 1}},
+				"env": bson.M{
+					"studyPlaceID": studyPlaceID,
+					"startDate":    startWeekDate,
+					"endDate": bson.M{
+						"$dateAdd": bson.M{
+							"startDate": startWeekDate,
+							"unit":      "week",
+							"amount":    "$weeksCount",
+						},
+					},
+					"weeksAmount": "$weeksCount",
 				},
 			},
 		},
 		bson.M{
 			"$lookup": bson.M{
-				"from": "GeneralLessons",
-				"let":  bson.M{"weekIndex": "$indexes.weekIndex", "dayIndex": "$indexes.dayIndex", "date": "$date"},
+				"from": "Lessons",
+				"let":  bson.M{"env": "$env"},
 				"pipeline": bson.A{
 					bson.M{
 						"$match": bson.M{
 							"$expr": bson.M{
 								"$and": bson.A{
-									bson.M{"$eq": bson.A{"$" + type_, typeName}},
-									bson.M{"$eq": bson.A{"$weekIndex", "$$weekIndex"}},
-									bson.M{"$eq": bson.A{"$dayIndex", "$$dayIndex"}},
-									bson.M{"$eq": bson.A{"$studyPlaceId", studyPlaceId}},
+									bson.M{
+										"$eq": bson.A{"$studyPlaceId", "$$env.studyPlaceID"},
+									}, bson.M{
+										"$eq": bson.A{"$" + type_, typeName},
+									}, bson.M{
+										"$gte": bson.A{"$startDate", "$$env.startDate"},
+									}, bson.M{
+										"$lt": bson.A{"$endDate", "$$env.endDate"},
+									},
 								},
 							},
 						},
-					}, bson.M{
+					},
+					bson.M{
 						"$addFields": bson.M{
-							"isGeneral": true,
-							"startDate": bson.M{"$toDate": bson.M{"$concat": bson.A{bson.M{"$dateToString": bson.M{"format": "%Y-%m-%d", "date": "$$date"}}, "T", "$startTime"}}},
-							"endDate":   bson.M{"$toDate": bson.M{"$concat": bson.A{bson.M{"$dateToString": bson.M{"format": "%Y-%m-%d", "date": "$$date"}}, "T", "$endTime"}}},
+							"isGeneral": false,
+						},
+					},
+				},
+				"as": "lessons",
+			},
+		},
+		bson.M{
+			"$addFields": bson.M{
+				"env.lastUpdatedDate": bson.M{"$max": "$lessons.endDate"},
+			},
+		},
+		bson.M{
+			"$addFields": bson.M{
+				"env.startGeneral": bson.M{
+					"$dateFromParts": bson.M{
+						"year":  bson.M{"$year": "$env.lastUpdatedDate"},
+						"month": bson.M{"$month": "$env.lastUpdatedDate"},
+						"day":   bson.M{"$sum": bson.A{bson.M{"$dayOfMonth": "$env.lastUpdatedDate"}, 1}},
+					},
+				},
+			},
+		},
+		bson.M{
+			"$addFields": bson.M{
+				"env.startWeekIndex":        bson.M{"$mod": bson.A{bson.M{"$isoWeek": "$env.startDate"}, "$env.weeksAmount"}},
+				"env.startGeneralDayIndex":  bson.M{"$subtract": bson.A{bson.M{"$isoDayOfWeek": "$env.startGeneral"}, 1}},
+				"env.startGeneralWeekIndex": bson.M{"$mod": bson.A{bson.M{"$isoWeek": "$env.startGeneral"}, "$env.weeksAmount"}},
+				"env.endGeneralDayIndex":    bson.M{"$subtract": bson.A{bson.M{"$isoDayOfWeek": "$env.endDate"}, 1}},
+				"env.endGeneralWeekIndex":   bson.M{"$mod": bson.A{bson.M{"$isoWeek": "$env.endDate"}, "$env.weeksAmount"}},
+			},
+		},
+		bson.M{
+			"$lookup": bson.M{
+				"from": "GeneralLessons",
+				"let":  bson.M{"env": "$env"},
+				"pipeline": bson.A{
+					bson.M{
+						"$match": bson.M{
+							"$expr": bson.M{
+								"$and": bson.A{
+									bson.M{
+										"$eq": bson.A{"$studyPlaceId", "$$env.studyPlaceID"},
+									}, bson.M{
+										"$eq": bson.A{"$" + type_, typeName},
+									},
+								},
+							},
+						},
+					},
+					bson.M{
+						"$addFields": bson.M{
+							"date": bson.M{
+								"$dateAdd": bson.M{
+									"startDate": bson.M{
+										"$dateAdd": bson.M{
+											"startDate": "$$env.startDate",
+											"unit":      "week",
+											"amount":    bson.M{"$abs": bson.M{"$subtract": bson.A{"$weekIndex", "$$env.startWeekIndex"}}},
+										},
+									},
+									"unit":   "day",
+									"amount": "$dayIndex",
+								},
+							},
+						},
+					},
+					bson.M{
+						"$match": bson.M{
+							"$expr": bson.M{
+								"$and": bson.A{
+									bson.M{"$gte": bson.A{"$date", "$$env.startGeneral"}},
+									bson.M{"$lt": bson.A{"$date", "$$env.endDate"}},
+								},
+							},
+						},
+					},
+					bson.M{
+						"$addFields": bson.M{
+							"startDate": bson.M{
+								"$toDate": bson.M{
+									"$concat": bson.A{bson.M{
+										"$dateToString": bson.M{
+											"format": "%Y-%m-%d",
+											"date":   "$date",
+										},
+									}, "T", "$startTime"},
+								},
+							},
+							"endDate": bson.M{
+								"$toDate": bson.M{
+									"$concat": bson.A{bson.M{
+										"$dateToString": bson.M{
+											"format": "%Y-%m-%d",
+											"date":   "$date",
+										},
+									}, "T", "$endTime"},
+								},
+							},
+							"isGeneral": false,
 						},
 					},
 				},
@@ -91,54 +191,34 @@ func (s *scheduleRepository) GetSchedule(ctx context.Context, studyPlaceId primi
 			},
 		},
 		bson.M{
-			"$lookup": bson.M{
-				"from": "Lessons",
-				"let":  bson.M{"date": "$date"},
-				"pipeline": bson.A{
-					bson.M{
-						"$match": bson.M{
-							"$expr": bson.M{
-								"$and": bson.A{
-									bson.M{"$eq": bson.A{bson.M{"$dateToString": bson.M{"format": "%Y-%m-%d", "date": "$startDate"}}, bson.M{"$dateToString": bson.M{"format": "%Y-%m-%d", "date": "$$date"}}}},
-									bson.M{"$eq": bson.A{"$" + type_, typeName}},
-									bson.M{"$eq": bson.A{"$studyPlaceId", studyPlaceId}},
-								},
-							},
-						},
-					}, bson.M{
-						"$addFields": bson.M{
-							"isGeneral": false,
-							"startDate": "$startDate",
-							"endDate":   "$endDate",
-						},
-					},
-				},
-				"as": "lessons",
+			"$addFields": bson.M{
+				"lessons": bson.M{"$concatArrays": bson.A{"$lessons", "$general"}},
 			},
 		},
-		bson.M{"$addFields": bson.M{"lessons": bson.M{"$cond": bson.A{bson.M{"$eq": bson.A{"$lessons", bson.A{}}}, "$general", "$lessons"}}}},
-		bson.M{"$unwind": "$lessons"},
-		bson.M{
-			"$group": bson.M{
-				"_id": nil,
-				"studyPlace": bson.M{"$first": bson.M{
-					"_id":        "$_id",
-					"name":       "$name",
-					"weeksCount": "$weeksCount",
-				}},
-				"lessons": bson.M{"$push": "$lessons"},
-			},
-		},
-		bson.M{"$sort": bson.M{"lessons.startDate": 1}},
 		bson.M{
 			"$addFields": bson.M{
+				"_id": nil,
 				"info": bson.M{
-					"startWeekDate": startWeekDate,
-					"date":          time.Now(),
+					"studyPlace":    "$$ROOT",
 					"type":          type_,
 					"typeName":      typeName,
-					"studyPlace":    "$studyPlace",
+					"startWeekDate": startWeekDate,
+					"date":          time.Now(),
 				},
+				"lessons": "$lessons",
+			},
+		},
+		bson.M{
+			"$project": bson.M{
+				"info.studyPlace.lessons": 0,
+				"info.studyPlace.general": 0,
+				"info.studyPlace.env":     0,
+			},
+		},
+		bson.M{
+			"$project": bson.M{
+				"info":    1,
+				"lessons": 1,
 			},
 		},
 	})
@@ -148,7 +228,7 @@ func (s *scheduleRepository) GetSchedule(ctx context.Context, studyPlaceId primi
 
 	if !cursor.Next(ctx) {
 		var studyPlace entities.StudyPlace
-		if err = s.studyPlacesCollection.FindOne(ctx, bson.M{"_id": studyPlaceId}).Decode(&studyPlace); err != nil {
+		if err = s.studyPlacesCollection.FindOne(ctx, bson.M{"_id": studyPlaceID}).Decode(&studyPlace); err != nil {
 			return entities.Schedule{}, err
 		}
 
---
+++ sql/getScheduleQuery.sql
@@ -0,0 +1,188 @@
+let studyPlaceID = new ObjectId("631261e11b8b855cc75cec35")
+let type = "group"
+let typename = "Т-095"
+let startWeekDate = new Date(2022, 9, 5)
+
+db.StudyPlaces.aggregate(
+    {
+        $match: {
+            "_id": studyPlaceID
+        }
+    },
+    {
+        $addFields: {
+            "env": {
+                "studyPlaceID": studyPlaceID,
+                "startDate": startWeekDate,
+                "endDate": {
+                    $dateAdd: {
+                        "startDate": startWeekDate,
+                        "unit": "week",
+                        "amount": "$weeksCount",
+                    }
+                },
+                "weeksAmount": "$weeksCount"
+            }
+        }
+    },
+    {
+        $lookup: {
+            "from": "Lessons",
+            "let": {"env": "$env"},
+            "pipeline": [
+                {
+                    $match: {
+                        $expr: {
+                            $and: [
+                                {
+                                    $eq: ["$studyPlaceId", "$$env.studyPlaceID"],
+                                }, {
+                                    $eq: ['$' + type, typename],
+                                }, {
+                                    $gte: ["$startDate", "$$env.startDate"],
+                                }, {
+                                    $lt: ["$endDate", "$$env.endDate"],
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    $addFields: {
+                        "isGeneral": false
+                    }
+                }
+            ],
+            "as": "lessons"
+        }
+    },
+    {
+        $addFields: {
+            "env.lastUpdatedDate": {$max: "$lessons.endDate"}
+        }
+    },
+    {
+        $addFields: {
+            "env.startGeneral": {
+                $dateFromParts: {
+                    'year': {$year: "$env.lastUpdatedDate"},
+                    'month': {$month: "$env.lastUpdatedDate"},
+                    'day': {$sum: [{$dayOfMonth: "$env.lastUpdatedDate"}, 1]},
+                }
+            }
+        }
+    },
+    {
+        $addFields: {
+            "env.startWeekIndex": {$mod: [{$isoWeek: "$env.startDate"}, "$env.weeksAmount"]},
+            "env.startGeneralDayIndex": {$subtract: [{$isoDayOfWeek: "$env.startGeneral"}, 1]},
+            "env.startGeneralWeekIndex": {$mod: [{$isoWeek: "$env.startGeneral"}, "$env.weeksAmount"]},
+            "env.endGeneralDayIndex": {$subtract: [{$isoDayOfWeek: "$env.endDate"}, 1]},
+            "env.endGeneralWeekIndex": {$mod: [{$isoWeek: "$env.endDate"}, "$env.weeksAmount"]},
+        }
+    },
+    {
+        $lookup: {
+            "from": "GeneralLessons",
+            "let": {"env": "$env"},
+            "pipeline": [
+                {
+                    $match: {
+                        $expr: {
+                            $and: [
+                                {
+                                    $eq: ["$studyPlaceId", "$$env.studyPlaceID"],
+                                }, {
+                                    $eq: ['$' + type, typename],
+                                },
+                            ]
+                        }
+                    }
+                },
+                {
+                    $addFields: {
+                        "date": {
+                            $dateAdd: {
+                                "startDate": {
+                                    $dateAdd: {
+                                        "startDate": "$$env.startDate",
+                                        "unit": "week",
+                                        "amount": {$abs: {$subtract: ["$weekIndex", "$$env.startWeekIndex"]}}
+                                    }
+                                },
+                                "unit": "day",
+                                "amount": "$dayIndex"
+                            }
+                        }
+                    }
+                },
+                {
+                    $match: {
+                        $expr: {
+                            $and: [
+                                {$gte: ["$date", "$$env.startGeneral"]},
+                                {$lt: ["$date", "$$env.endDate"]}
+                            ]
+                        }
+                    }
+                },
+                {
+                    $addFields: {
+                        "startDate": {
+                            $toDate: {
+                                $concat: [{
+                                    $dateToString: {
+                                        "format": "%Y-%m-%d",
+                                        "date": "$date"
+                                    }
+                                }, "T", "$startTime"]
+                            }
+                        },
+                        "endDate": {
+                            $toDate: {
+                                $concat: [{
+                                    $dateToString: {
+                                        "format": "%Y-%m-%d",
+                                        "date": "$date"
+                                    }
+                                }, "T", "$endTime"]
+                            }
+                        },
+                        "isGeneral": false
+                    }
+                },
+            ],
+            "as": "general"
+        }
+    },
+    {
+        $addFields: {
+            "lessons": {$concatArrays: ["$lessons", "$general"]}
+        }
+    },
+    {
+        $addFields: {
+            "_id": null,
+            "info": {
+                "studyPlace": "$$ROOT",
+                "type": type,
+                "typeName": typename,
+                "startWeekDate": startWeekDate
+            },
+            "lessons": "$lessons"
+        }
+    },
+    {
+        $project: {
+            "studyPlace.lessons": 0,
+            "studyPlace.general": 0,
+            "studyPlace.env": 0
+        }
+    },
+    {
+        $project: {
+            "studyPlace": 1,
+            "lessons": 1,
+        }
+    }
+    )
