ed5f2c3f0af7babb7da5a18fd4240e486e0f87fd
LikDan <likdan.company@gmail.com>
2022-09-27 19:22:42 +0300

    Improve select with code

--- internal/entities/user.go
+++ internal/entities/user.go
@@ -12,8 +12,8 @@ type User struct {
 	Name          string             `json:"name" bson:"name" encryption:""`
 	PictureUrl    string             `json:"picture" bson:"picture" encryption:""`
 	Type          string             `json:"type" bson:"type"`
-	TypeName      string             `json:"typeName" bson:"typeName"`
-	StudyPlaceId  primitive.ObjectID `json:"studyPlaceId" bson:"studyPlaceId"`
+	TypeName      string             `json:"typeName" bson:"typename"`
+	StudyPlaceId  primitive.ObjectID `json:"studyPlaceId" bson:"studyPlaceID"`
 	Permissions   []string           `json:"permissions" bson:"permissions"`
 	Accepted      bool               `json:"accepted" bson:"accepted"`
 	Blocked       bool               `json:"blocked" bson:"blocked"`
