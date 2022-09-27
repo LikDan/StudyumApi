7a9461322e646bc9b0f62ca3f382ef51b3c8e01e
likdan <likdan.company@gmail.com>
2022-09-27 19:36:59 +0300

    Add validation

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -3,12 +3,12 @@ package dto
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type UserLoginDTO struct {
-	Login    string `json:"login" binding:"required"`
+	Login    string `json:"login" binding:"excludesall= ,required"`
 	Password string `json:"password" binding:"min=8"`
 }
 
 type UserSignUpDTO struct {
-	Login    string `json:"login" binding:"required"`
+	Login    string `json:"login" binding:"excludesall= ,required"`
 	Name     string `json:"name" binding:"required"`
 	Email    string `json:"email" binding:"email"`
 	Password string `json:"password" binding:"min=8"`
@@ -22,7 +22,7 @@ type UserSignUpWithCodeDTO struct {
 }
 
 type EditUserDTO struct {
-	Login    string `json:"login" binding:"required"`
+	Login    string `json:"login" binding:"excludesall= ,required"`
 	Email    string `json:"email" binding:"email"`
 	Password string `json:"password" binding:"min=8|eq="`
 }
